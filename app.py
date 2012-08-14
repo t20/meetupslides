feb2df012894066a26c81b80f4910e0f053ea1b6
Bharad <barbi.bruce@gmail.com>
2012-08-13 22:38:42 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+################################
+####### helper methods #########
+################################
+
+
+################################
+####### All router methods #####
+################################
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'GET'
+        return render_template('add.html')
+    # else - POST
+    # TODO look into flask wtf
+    # get post data and add to database
+    post_id = None
+    return redirect(url_for('post', post_id=post_id))
+
+
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    if request.method == 'GET':
+        post = get_post(post_id)
+        return render_template('post.html', post=post)
+    # else  - POST
+    # update_post()
+    return render_template('post.html', post=post)
+
+
+# TODO - research flask login
+@app.route('/login')
+def login():
+    """docstring for login"""
+    pass
+
+
+@app.route('/logout')
+def logout():
+    """docstring for logout"""
+    pass
+
+
+@app.route('/register')
+def register():
+    """docstring for register"""
+    pass
+
+
+@app.route('/profile')
+def profile():
+    """docstring for profile"""
+    pass
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact(post_id):
+    if request.method == 'GET':
+        return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
