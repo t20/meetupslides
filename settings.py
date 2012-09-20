5bd6b37cd753683ca803c2e111a6a6e66736a965
Bharad <bharad@brightscope.com>
2012-09-19 22:13:21 -0700

    Rename config to settings. Added redis connection params

--- settings.py
+++ settings.py
@@ -3,8 +3,12 @@ class Config(object):
     DEBUG = True
 
     APP_SECRET_KEY = ''
-
     
+    REDIS_DB = 0
+    REDIS_PORT = 6379
+    REDIS_HOST = 'localhost'
+    
+    r = None    
 
 class ProductionConfig(Config):
     DEBUG = False
