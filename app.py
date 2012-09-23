05c4d34e92639a64b6b4434529e6ca94556ec6d9
Bharad <bharad@brightscope.com>
2012-09-23 12:09:00 -0700

    jobs views and templates stub

--- app.py
+++ app.py
@@ -117,6 +117,12 @@ def contact(post_id):
         return render_template('contact.html')
 
 
+@app.route('/jobs')
+def jobs():
+    jobs = get_jobs()
+    return render_template('jobs.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
