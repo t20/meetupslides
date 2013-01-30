99e21a7fc2a8f055fc82eb9712e19578dfc3e95e
Bharad <barbi.bruce@gmail.com>
2013-01-29 22:55:24 -0800

    explicitly specify debug as false when running in production mode

--- app.py
+++ app.py
@@ -221,6 +221,7 @@ def jobs():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 0))
     if port:
+        app.debug = False
         app.run(host='0.0.0.0', port=port)
     else:
         app.debug = True
