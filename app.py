3e3a0ee08489aa5ce947b1bcda7005bb8faca851
Bharad <barbi.bruce@gmail.com>
2013-02-03 22:39:56 -0800

    About page - view, template and navbar link

--- app.py
+++ app.py
@@ -224,6 +224,11 @@ def contact():
     return redirect(url_for(contact))
 
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+
 @app.route('/jobs')
 def jobs():
     jobs = get_jobs()
