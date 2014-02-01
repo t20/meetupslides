b3f3d484a368ee46904331d2af1925379334ebe3
Madan Thangavelu <madan.thangavelu@lookout.com>
2014-01-31 22:16:49 -0800

    Real meeetup additions in sanfrancisco

--- default_meetups.py
+++ default_meetups.py
@@ -1,15 +1,74 @@
 DEFAULT_MEETUPS = [
 {
-"city": "SanFrancisco",
-"name": "Ruby",
-"website": "http://ruby.com",
-"desc": "This is a ruby meetup in sanfrancisco"
+"city": "San Francisco",
+"name": "SFHTML5",
+"website": "http://www.meetup.com/sfhtml5/",
+"desc": ""
 },                   
 
 {
-"city": "SanDiego",
-"name": "Python",
-"website": "http://ruby.com",
-"description": "This is a python meetup in Sandiego"
-},                                                                                          
+"city": "San Francisco",
+"name": "The San Francisco Ruby Meetup Group",
+"website": "http://www.meetup.com/sfruby/",
+"description": ""
+},              
+
+{
+"city": "San Francisco",
+"name": "Bay Area HBase User Group",
+"website": "http://www.meetup.com/hbaseusergroup/",
+"description": ""
+}, 
+      
+{
+"city": "San Francisco",
+"name": "The SF Javascript Meetup",
+"website": "http://www.meetup.com/jsmeetup/",
+"description": ""
+},     
+                   
+{
+"city": "San Francisco",
+"name": "iOS Developers (SF/SV)",
+"website": "http://www.meetup.com/ios-developers/",
+"description": ""
+},     
+
+{
+"city": "San Francisco",
+"name": "San Francisco & Silicon Valley Web Performance Group",
+"website": "http://www.meetup.com/SF-Web-Performance-Group/",
+"description": ""
+},     
+
+
+{
+"city": "San Francisco",
+"name": "San Francisco Tech4Good",
+"website": "http://www.meetup.com/sfnetsquared/",
+"description": ""
+},     
+                                    
+{
+"city": "San Francisco",
+"name": "Lean Startup Circle",
+"website": "http://www.meetup.com/Lean-Startup-Circle/",
+"description": ""
+},                    
+                   
+{
+"city": "San Francisco",
+"name": "Big Data Silicon Valley",
+"website": "http://www.meetup.com/Big-Data-Silicon-Valley/",
+"description": ""
+},                     
+                   
+{
+"city": "San Francisco",
+"name": "Learning Android Development",
+"website": "http://www.meetup.com/Learning-Android-Development/",
+"description": ""
+},                     
+                         
+                                                                                                                                            
 ]
